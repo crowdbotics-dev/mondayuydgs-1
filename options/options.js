98dc0f16f5e1bc7b93d943069bcffcf598937c22
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 10:02:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mondayuydgs-1",
+  url: "https://mondayuydgs-1.botics.co",
+  api: "https://mondayuydgs-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
